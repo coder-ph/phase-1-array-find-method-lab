cb4a54c1e7d3b450e40d32f7bc1ae28dcc758963
Phelix <phelixmbani@gmail.com>
2024-09-30 13:43:46 +0300

    Array find method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(winRecord){
+    for(const winStatus of winRecord){
+        if(winStatus.result === "W"){
+            return winStatus.year;
+        }
+    }
+}
+
+record.find(superbowlWin())
